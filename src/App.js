a1f89aac66da5636ff8a90aa0daba174dd120d8e
Simo Siren <simo.siren@stud.pointcollege.fi>
2021-07-05 01:08:46 +0300

    login ok but token not delivered to queries yet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { useState } from 'react'
+import { useApolloClient } from '@apollo/client'
 
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import LoginForm from './components/LoginForm'
+
+
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
 
   return (
     <div>
@@ -13,6 +25,10 @@ const App = () => {
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
+        <button onClick={() => setPage('login')}>login</button>
+        <button onClick={logout}>
+          logout
+        </button>
       </div>
 
       <Authors
@@ -25,6 +41,10 @@ const App = () => {
 
       <NewBook
         show={page === 'add'} setPage={setPage}
+      />
+
+      <LoginForm
+        show={page === 'login'} setPage={setPage} setToken={setToken}
       />
 
     </div>
